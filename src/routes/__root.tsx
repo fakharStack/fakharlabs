eaf2c79b3390fff4ea668fb375fdc923d1f64bc1
fakharStack <fakhar436gh@gmail.com>
2026-08-17 18:06:26 +0500

    Fix favicon link to use logo.png

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -77,7 +77,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/logo,png", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
